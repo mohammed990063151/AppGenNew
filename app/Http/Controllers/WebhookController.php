60e45e0c58445374c204a14393c5e7a737649265
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-05 01:03:01 -0700

    new Sala package

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Salla\OAuth2\Client\Provider\Salla;
 
 class WebhookController extends Controller
 {
     public function handle(Request $request){
-        info('web hook is verfiy jksa is a good man');
-        info("$request");
+
+        $provider = new Salla([
+            'clientId'     => '21dcaa144ad4807e1132a0be3c26dca5', // The client ID assigned to you by Salla
+            'clientSecret' => 'a0d88f3d870efe82fc3e177c4440364c', // The client password assigned to you by Salla
+            'redirectUri'  => 'https://appgen.themsc.net/admin/webhook', // the url for current page in your service
+        ]);
+
+        // info('web hook is verfiy jksa is a good man');
+        // info("$request");
+        try{
+
+            $token = $provider->getAccessToken('authorization_code', [
+                'code' => $_GET['code']
+            ]);
+
+            //
+            // ## Access Token
+            //
+            // You should store the access token
+            // which may use in authenticated requests against the Salla's API
+            echo 'Access Token: '.$token->getToken()."<br>";
+
+
+            $response = $provider->fetchResource(
+                'GET',
+                'https://api.salla.dev/admin/v2/orders',
+                $token->getToken()
+            );
+            dd($response);
+            // var_export($response);
+
+        }
+        catch(\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e){
+            exit($e->getMessage());
+        }
         return $request;
     }
 }

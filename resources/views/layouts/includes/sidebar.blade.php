c152d300266ef82df6b226ae0e104d306a809322
haithmAlhakmabe <101138301+haithmAlhakmabe@users.noreply.github.com>
2022-04-18 12:45:25 +0200

    Update sidebar.blade.php

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -70,7 +70,20 @@
 
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+                        <i class="ni ni-tv-2 text-pri
+                        
+
+
+
+
+
+
+
+
+
+
+
+                        
                     </div>
                     <span class="nav-link-text ms-1">{{_('translation.Technical support')}}</span>
                 </a>

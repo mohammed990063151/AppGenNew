e4c16df723567af8b216e4e9b98c268b3127ab64
haithmAlhakmabe <101138301+haithmAlhakmabe@users.noreply.github.com>
2022-05-22 10:16:23 +0200

    env 2

---
+++ resources/views/layouts/Admin/includes/nav.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl me-4 {{getclass([' ar'=> 'ps ps__rtl','en'=>'' ])}} " id="navbarBlur"
+    data-scroll="false">
+    <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+            
+            {{-- @endSection --}}
+<div class="collapse navbar-collapse" id="navbar-mobile">
+    <ul class="nav navbar-nav mr-auto float-left">
+        <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs is-active" href="#"><i class="ft-menu"></i></a></li>
+
+    </ul>
+    <ul class="nav navbar-nav float-right">
+        <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-gb"></i><span class="selected-language"></span></a>
+            <div class="dropdown-menu" aria-labelledby="dropdown-flag">
+               
+
+            </div>
+        </li>
+        <li class="dropdown dropdown-user nav-item">
+            <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
+                <span class="mr-1">
+                    <span class="user-name text-bold-700">Haithm alhakmabe </span>
+                </span>
+                <span class="avatar avatar-online">
+            </a>
+           
+                
+               
+        </li>
+
+        
+    </ul>
+</div>
+</nav>

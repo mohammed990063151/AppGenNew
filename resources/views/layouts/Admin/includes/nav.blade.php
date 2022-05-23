345d0ffc0d4fd8bf8625ec69b4f4e154a3ce5310
haithmAlhakmabe <101138301+haithmAlhakmabe@users.noreply.github.com>
2022-05-23 08:53:45 +0200

    nav admin

--- resources/views/layouts/Admin/includes/nav.blade.php
+++ resources/views/layouts/Admin/includes/nav.blade.php
@@ -1,34 +1,31 @@
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl me-4 {{getclass([' ar'=> 'ps ps__rtl','en'=>'' ])}} " id="navbarBlur"
-    data-scroll="false">
-    <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
-            @yield()
-            {{-- @endSection --}}
-<div class="collapse navbar-collapse" id="navbar-mobile">
-    <ul class="nav navbar-nav mr-auto float-left">
-        <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs is-active" href="#"><i class="ft-menu"></i></a></li>
-
-    </ul>
-    <ul class="nav navbar-nav float-right">
-        <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-gb"></i><span class="selected-language"></span></a>
-            <div class="dropdown-menu" aria-labelledby="dropdown-flag">
-               
-
-            </div>
-        </li>
-        <li class="dropdown dropdown-user nav-item">
-            <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                <span class="mr-1">
-                    <span class="user-name text-bold-700">Haithm alhakmabe </span>
-                </span>
-                <span class="avatar avatar-online">
-            </a>
-           
+<div class="card" style="padding:inherit;" >
+    <div class="card-body " style="padding:inherit;">
+        <div class="d-flex justify-content-between" >
+            <div class="icon m-3" style=""><i class="bi bi-menu-down  "></i></div>
+            
+            <div class="m-2">
+                  <span class="">{{Auth::user()->name}}</span>
+                  <img src="https://cdn-icons-png.flaticon.com/512/147/147142.png" style="height: 30px;width: 30px;"  alt="">
                 
-               
-        </li>
+                </div>
+                </div>
+        </div>
+    </div>
 
-        
-    </ul>
 </div>
-</nav>
+
+
+
+{{-- <i class="flag-icon flag-icon-gb"></i> --}}
+<li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link"
+    id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true"
+    aria-expanded="false"><i class="flag-icon flag-icon-gb"></i><span
+        class="selected-language"></span></a>
+<div class="dropdown-menu" aria-labelledby="dropdown-flag">
+    <a class="dropdown-item" href="https://faster.themsc.net/locale/en"><i
+            class="flag-icon flag-icon-gb"></i> English</a>
+    <a class="dropdown-item" href="https://faster.themsc.net/locale/ar"><i
+            class="flag-icon flag-icon-sa"></i> العربيه</a>
+
+</div>
+</li>

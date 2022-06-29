40812f63d8ddc19f7647211b8eafb5b62bff0c67
haithmAlhakmabe <101138301+haithmAlhakmabe@users.noreply.github.com>
2022-06-29 10:20:25 +0200

    admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,7 @@
                                 </div>
                                 <div class="col-md-8">
                                     <h6 class="text-muted font-semibold">{{ __('translation.ticket') }}</h6>
-                                    <h6 class="font-extrabold mb-0">112.000</h6>
+                                    <h6 class="font-extrabold mb-0">1</h6>
                                 </div>
                             </div>
                         </div>
@@ -33,7 +33,7 @@
                                 </div>
                                 <div class="col-md-8">
                                     <h6 class="text-muted font-semibold">{{ __('translation.App') }}</h6>
-                                    <h6 class="font-extrabold mb-0">183.000</h6>
+                                    <h6 class="font-extrabold mb-0">0</h6>
                                 </div>
                             </div>
                         </div>
@@ -52,7 +52,7 @@
                                 </div>
                                 <div class="col-md-8">
                                     <h6 class="text-muted font-semibold">{{ __('translation.Application') }}</h6>
-                                    <h6 class="font-extrabold mb-0">80.000</h6>
+                                    <h6 class="font-extrabold mb-0">1</h6>
                                 </div>
                             </div>
                         </div>
@@ -71,7 +71,7 @@
                                 </div>
                                 <div class="col-md-8">
                                     <h6 class="text-muted font-semibold">{{ __('translation.notifcation') }}</h6>
-                                    <h6 class="font-extrabold mb-0">112</h6>
+                                    <h6 class="font-extrabold mb-0">0</h6>
                                 </div>
                             </div>
                         </div>
@@ -224,10 +224,10 @@
                 <div class="card-content pb-4">
                     <div class="recent-message d-flex px-4 py-3">
                         <div class="avatar avatar-lg">
-                            <svg style="width:50px;height:50px" viewBox="0 0 24 24">
+                            <svg style="width:30px;height:30px" viewBox="0 0 24 24">
                                 <path fill="currentColor" d="M9,22A1,1 0 0,1 8,21V18H4A2,2 0 0,1 2,16V4C2,2.89 2.9,2 4,2H20A2,2 0 0,1 22,4V16A2,2 0 0,1 20,18H13.9L10.2,21.71C10,21.9 9.75,22 9.5,22V22H9M10,16V19.08L13.08,16H20V4H4V16H10M6,7H18V9H6V7M6,11H15V13H6V11Z" />
                             </svg>
-                            <h3>{{__("translation.Ticket")}}</h3>
+                            <h5>{{__("translation.Ticket")}}</h5>
                         </div>
                         <div class="name ms-4">
                             <h5 class="mb-1"></h5>
@@ -236,10 +236,10 @@
                     </div>
                     <div class="recent-message d-flex px-4 py-3">
                         <div class="avatar avatar-lg">
-                            <svg style="width:50px;height:50px;(28, 128, 0)" viewBox="0 0 24 24">
+                            <svg style="width:30px;height:30px;(28, 128, 0)" viewBox="0 0 24 24">
                                 <path fill="currentColor" d="M10.5 15.5C10.5 15.87 10.4 16.2 10.22 16.5C9.88 15.91 9.24 15.5 8.5 15.5S7.12 15.91 6.78 16.5C6.61 16.2 6.5 15.87 6.5 15.5C6.5 14.4 7.4 13.5 8.5 13.5S10.5 14.4 10.5 15.5M23 15V18C23 18.55 22.55 19 22 19H21V20C21 21.11 20.11 22 19 22H5C3.9 22 3 21.11 3 20V19H2C1.45 19 1 18.55 1 18V15C1 14.45 1.45 14 2 14H3C3 10.13 6.13 7 10 7H11V5.73C10.4 5.39 10 4.74 10 4C10 2.9 10.9 2 12 2S14 2.9 14 4C14 4.74 13.6 5.39 13 5.73V7H14C17.87 7 21 10.13 21 14H22C22.55 14 23 14.45 23 15M21 16H19V14C19 11.24 16.76 9 14 9H10C7.24 9 5 11.24 5 14V16H3V17H5V20H19V17H21V16M15.5 13.5C14.4 13.5 13.5 14.4 13.5 15.5C13.5 15.87 13.61 16.2 13.78 16.5C14.12 15.91 14.76 15.5 15.5 15.5S16.88 15.91 17.22 16.5C17.4 16.2 17.5 15.87 17.5 15.5C17.5 14.4 16.61 13.5 15.5 13.5Z" />
                             </svg>
-                            <h3>{{__("translation.Application")}}</h3>
+                            <h6>{{__("translation.Application")}}</h6>
                         </div>
                         <div class="name ms-4">
                             <h5 class="mb-1"></h5>
@@ -248,10 +248,10 @@
                     </div>
                     <div class="recent-message d-flex px-4 py-3">
                         <div class="avatar avatar-lg">
-                            <svg style="width:50px;height:50px;" viewBox="0 0 24 24">
+                            <svg style="width:30px;height:30px;" viewBox="0 0 24 24">
                                 <path fill="currentColor" d="M21,19V20H3V19L5,17V11C5,7.9 7.03,5.17 10,4.29C10,4.19 10,4.1 10,4A2,2 0 0,1 12,2A2,2 0 0,1 14,4C14,4.1 14,4.19 14,4.29C16.97,5.17 19,7.9 19,11V17L21,19M14,21A2,2 0 0,1 12,23A2,2 0 0,1 10,21" />
                             </svg>
-                            <h3>{{__("translation.Notification")}}</h3>
+                            <h6>{{__("translation.Notification")}}</h6>
                         </div>
                         <div class="name ms-4">
                             <h5 class="mb-1"></h5>
